d7dd74e229108a0b7332269879eed8cc49973227
RambhuSingh1819 <156609631+RambhuSingh1819@users.noreply.github.com>
2026-02-04 10:01:04 +0530

    Update FoodDeliveryApplicationTests.java

--- src/test/java/com/fooddelivery/FoodDeliveryApplicationTests.java
+++ src/test/java/com/fooddelivery/FoodDeliveryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.fooddelivery;
+// package com.fooddelivery;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FoodDeliveryApplicationTests {
+// @SpringBootTest
+// class FoodDeliveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
